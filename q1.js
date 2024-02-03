5dd4cccc175a5f0d9a93697909466e690acb4389
Sanjay Saravanan <sanjaysaravanan38@gmail.com>
2024-02-03 13:01:57 +0530

    first commit

---
+++ q1.js
@@ -0,0 +1,23 @@
+// HTTP 1.1
+/* 
+
+Info about HTTP 1.1
+
+asjhdfklhaskfhlsahdfklas;dfka'sdflad
+sdfkhaskdfhklashdlfkhaksdhfasd
+lshdfkhaksdfhklashdkfhasd
+
+
+*/
+
+// HTTP 2
+/* 
+
+Info about HTTP 2
+
+asjhdfklhaskfhlsahdfklas;dfka'sdflad
+sdfkhaskdfhklashdlfkhaksdhfasd
+lshdfkhaksdfhklashdkfhasd
+
+
+*/
